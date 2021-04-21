61bc67de4d635cd38cd8c1e24c665e5715e895db
Saketh Nimmagadda <sakethn@cs.washington.edu>
2021-04-20 18:48:43 -0700

    Makes sure file type is edf and allows you to upload data from file to the backend, added axios to allow for file upload

--- api/api.py
+++ api/api.py
@@ -1,12 +1,15 @@
 import random
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route('/result')
+@app.route('/result', methods=['POST'])
 @cross_origin()
 def get_result():
+    # TODO: Eventually can get actual data not just filename. Encrypt it
+    file = request.files['file']
+    filename = file.filename
     return {'result': bool(random.getrandbits(1))}
--- src/components/main.js
+++ src/components/main.js
@@ -2,27 +2,52 @@ import { useState } from "react";
 
 export default function Header() {
   const [result, setResult] = useState(null);
+  const [file, setFile] = useState(null);
 
   const handleUpload = () => {
-    // TODO: pass input to the model
-    fetch("http://localhost:5000/result")
-      .then(res => res.json())
-      .then(data => {
-        setResult(data.result);
-      });
+    if (file) {
+      const data = new FormData();
+      data.append("file", file);
+      data.append("filename", file.name);
+
+      // TODO: Eventually can get actual data not just filename. Encrypt it
+      fetch("http://localhost:5000/result", {
+        method: "POST",
+        body: data
+      })
+        .then(res => res.json())
+        .then(data => {
+          setResult(data.result);
+        });
+    } else {
+      window.alert("No data selected!");
+    }
+  };
+
+  const onFileChange = event => {
+    setFile(event.target.files[0]);
   };
 
   return (
     <>
       <div className="mt-32 flex justify-between">
-        <button
-          className="ml-32 bg-white p-5 shadow-lg 
+        <div className="flex flex-col">
+          <button
+            className="ml-32 bg-white p-5 shadow-lg 
     bg-clip-padding bg-opacity-60 border border-gray-200
-     rounded-lg font-semibold"
-          onClick={handleUpload}
-        >
-          Upload Data
-        </button>
+     rounded-lg font-semibold w-40"
+            onClick={handleUpload}
+          >
+            Upload Data
+          </button>
+          <input
+            className="ml-32 mt-8"
+            type="file"
+            onChange={onFileChange}
+            accept=".edf"
+          />
+        </div>
+
         {result != null && (
           <h1 className="p-5 font-semibold">{`Result: ${result}`}</h1>
         )}
