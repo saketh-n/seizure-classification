b566436210b625ebae0f3665c65c299adfe92292
Saketh Nimmagadda <sakethn@cs.washington.edu>
2021-05-02 14:34:17 -0700

    Uploads data from file as binary string

--- api/api.py
+++ api/api.py
@@ -9,10 +9,9 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @app.route('/result', methods=['POST'])
 @cross_origin()
 def get_result():
-    # TODO: Eventually can get actual data not just filename. Encrypt it
-    file = request.files['file']
-    filename = file.filename
-    # TODO: dummy array of time-boxed seizure classifications
+    # Access the passed data in this manner. 
+    data = request.form['filedata']
+    # TODO: Eventually this dummy classifications array replaced w/ real deal
     results = []
     for i in range(10):
         results.append(random.random())
