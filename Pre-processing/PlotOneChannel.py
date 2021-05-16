ffe62eb6e560f9ec3cb069a1f8fcddcf4bfcb8c4
devyansh <37020063+devyansh@users.noreply.github.com>
2021-05-15 18:52:41 -0700

    generating plots for each channel individually
    2 plots for each Channel: one for RAW Data and other is Spectral Power Density
    
    PlotAllChannels plots and saves the images as well

--- Pre-processing/PlotOneChannel.py
+++ Pre-processing/PlotOneChannel.py
@@ -1,35 +1,51 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mne.io import concatenate_raws, read_raw_edf
+import os
 
 
 #Load Data
-load_path = 'C:/Users/User/Desktop/CSE 481C - Neruo Capstone/Project Repo/Data/TUH/00003010_s003_t000.edf'
+filename = '00003010_s003_t000'
+fileformat = '.edf'
+load_path = 'C:/Users/User/Desktop/CSE 481C - Neruo Capstone/Project Repo/Data/TUH/' + filename + fileformat
 print(load_path)
 raw = read_raw_edf(load_path, preload=True)
 
 #Inspect the .edf file
-print(raw.info['ch_names'])
-ch_name = raw.info['ch_names']
-print("Total Channels: ", len(ch_name))
+channel_names = raw.info['ch_names']
+len_channels = len(channel_names)
+print(channel_names)
+print("Total Channels: ", len_channels)
 
-#Select the required Channel
-requiredChannel = ch_name[1]
-raw.pick(requiredChannel)
+#Make a directory
+directory = filename
+parent_path = 'C:/Users/User/Desktop/CSE 481C - Neruo Capstone/Project Repo/Data/GeneratedPlots/'
+path = os.path.join(parent_path, directory)
+os.mkdir(path)
 
-#Plotting begins!
-data, times = raw[:, :]
-fig = plt.subplots(figsize=(10,8))
-plt.plot(times, data.T);
-plt.xlabel('Seconds')
-plt.ylabel('$\mu V$')
-plt.title('Channels: 1-5');
-plt.legend(raw.ch_names[1:2]);
-plt.show()
+def makePlots(i):
+    #Select the required Channel
+    requiredChannel = channel_names[i]
+    copy = raw.copy()
+    copy.pick(requiredChannel)
+
+    #Plotting begins!
+    data, times = copy[:, :]
+    fig = plt.figure()
+    plt.plot(times, data.T);
+    plt.xlabel('Seconds')
+    plt.ylabel('$\mu V$')
+    plt.title('Channel: ' + str(requiredChannel));
+    saveNameRAW = path + '/' + filename + '_' + requiredChannel + '_RAW'
+    plt.savefig(saveNameRAW, dpi = 300)
+    plt.close(fig)
 
 
-#Power spectrum
-raw.plot_psd(tmax=np.inf)
-plt.show()
+    #Power spectrum
+    psd_fig = copy.plot_psd(show=False)
+    saveNamePower = path + '/' + filename + '_' + requiredChannel + '_psd'
+    psd_fig.savefig(saveNamePower, dpi = 300)
 
-
+ran = range(1, len_channels)
+for i in ran:
+    makePlots(i)
